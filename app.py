ca8e65e4d6ef0bf5cc58a82f08d7c6c3ae400ba5
“lawrence1986” <madusquare@gmail.com>
2023-12-29 17:01:37 +0100

    update

--- app.py
+++ app.py
@@ -104,4 +104,4 @@ def create_data():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        app.run(debug=True, port=5004)
+        app.run(debug=False, port=5004)
